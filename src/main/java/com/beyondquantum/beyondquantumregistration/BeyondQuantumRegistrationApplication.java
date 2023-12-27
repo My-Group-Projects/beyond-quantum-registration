9f943827caea5b7d006c5d093d19a3219a52fc07
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2023-12-28 03:11:00 +0530

    Repo setup initial commit

---
+++ src/main/java/com/beyondquantum/beyondquantumregistration/BeyondQuantumRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.beyondquantum.beyondquantumregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondQuantumRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondQuantumRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyondquantum/beyondquantumregistration/BeyondQuantumRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondquantum.beyondquantumregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondQuantumRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
