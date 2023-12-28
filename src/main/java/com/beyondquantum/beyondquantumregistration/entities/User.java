105b1dc43c2419479d9c7d6e8e84f076e9e56626
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2023-12-29 00:43:04 +0530

    Added server side form validation

--- src/main/java/com/beyondquantum/beyondquantumregistration/entities/User.java
+++ src/main/java/com/beyondquantum/beyondquantumregistration/entities/User.java
@@ -2,16 +2,25 @@ package com.beyondquantum.beyondquantumregistration.entities;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class User {
     @NotBlank(message = "Name cannot be blank")
     private String name;
     @NotBlank(message = "Email cannot be blank")
+    @Pattern(regexp = "^[\\w.-]+@[a-zA-Z\\d.-]+\\.[a-zA-Z]{2,}$", message = "Invalid Email Address")
     private String email;
+    @Pattern(regexp = "[0-9]{10}", message = "Invalid Phone Number")
     @NotBlank(message = "Phone cannot be blank")
     private String phone;
+
+    // Username: Only lowercase letters, numbers, and underscores, cannot start with a number
+    @Pattern(regexp = "^(?=(?:[^a-z]*[a-z]){3})[a-z\\d_]{5,}$", message = "Username must be at least 5 characters long, may contain underscores, may contain numbers, only small letters are allowed, and should have at least 3 letters.")
     @NotBlank(message = "Username cannot be blank")
     private String username;
+
+    // At least one letter, one digit, one capital letter, one small letter, and one special character
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Your password must be atleast 8 characters long, contain atleast one Capital, Small Letter, Special Character and a Number.")
     @NotBlank(message = "Please set a password")
     private String password;
 
