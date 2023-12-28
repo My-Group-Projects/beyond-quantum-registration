000a4de47229a013426a77e0ab12f5c913956032
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2023-12-28 17:48:59 +0530

    Added request mapping for success

--- src/main/java/com/beyondquantum/beyondquantumregistration/controller/RegistrationController.java
+++ src/main/java/com/beyondquantum/beyondquantumregistration/controller/RegistrationController.java
@@ -1,14 +1,20 @@
 package com.beyondquantum.beyondquantumregistration.controller;
 
+import com.beyondquantum.beyondquantumregistration.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
     @GetMapping("/registration")
-    public String registrationForm(Model model){
-        model.addAttribute("name", "Abhay Bhandari");
+    public String registrationForm(){
         return "registrationForm";
+    }
+
+    @PostMapping("/success")
+    public String onSubmit(){
+        return "success";
     }
 }
