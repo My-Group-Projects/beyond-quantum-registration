72bc274d833021acd5411f39042bf20c23307a8b
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2024-03-18 00:50:11 +0530

    Created user entity

--- src/main/java/com/beyondquantum/beyondquantumregistration/entities/User.java
+++ src/main/java/com/beyondquantum/beyondquantumregistration/entities/User.java
@@ -1,20 +1,38 @@
 package com.beyondquantum.beyondquantumregistration.entities;
 
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import java.util.UUID;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 6 digit user id
+    @Column(unique = true)
+    private String user_id;
     @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Column(unique = true)
     @NotBlank(message = "Email cannot be blank")
     @Pattern(regexp = "^[\\w.-]+@[a-zA-Z\\d.-]+\\.[a-zA-Z]{2,}$", message = "Invalid Email Address")
     private String email;
+    @Column(unique = true)
     @Pattern(regexp = "[0-9]{10}", message = "Invalid Phone Number")
     @NotBlank(message = "Phone cannot be blank")
     private String phone;
 
     // Username: Only lowercase letters, numbers, and underscores, cannot start with a number
+    @Column(unique = true)
     @Pattern(regexp = "^(?=(?:[^a-z]*[a-z]){3})[a-z\\d_]{5,}$", message = "Username must be at least 5 characters long, may contain underscores, may contain numbers, only small letters are allowed, and should have at least 3 letters.")
     @NotBlank(message = "Username cannot be blank")
     private String username;
@@ -73,7 +91,13 @@ public class User {
         this.password = password;
     }
 
-    public User(){
+    public User() {
         super();
+    }
+
+    @PrePersist
+    public void generateUserId() {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        this.user_id = uuid.substring(0, 6);
     }
 }
