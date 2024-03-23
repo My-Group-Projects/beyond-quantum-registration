e4d10ec0a6d4107b6cbb2f17a2ee52f34885c7e6
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2024-03-23 21:41:21 +0530

    Added User Service Interface

--- src/main/java/com/beyondquantum/beyondquantumregistration/service/UserService.java
+++ src/main/java/com/beyondquantum/beyondquantumregistration/service/UserService.java
@@ -1,16 +1,7 @@
 package com.beyondquantum.beyondquantumregistration.service;
 
-import com.beyondquantum.beyondquantumregistration.entities.User;
-import com.beyondquantum.beyondquantumregistration.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.beyondquantum.beyondquantumregistration.dto.UserDto;
 
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-
-    public void addUserToDatabase(User user) {
-        userRepository.save(user);
-    }
+public interface UserService {
+    public void addUserToDatabase(UserDto userDto);
 }
